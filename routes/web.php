d2d1415d832a11dd06a0122d63fd83dfd76b6403
Leoka2000 <reusoliveiraleo@gmail.com>
2024-01-21 19:01:42 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Note;
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +21,35 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+    Route::view('students/createProfile', 'notes.create')
+    ->middleware(['auth'])
+    ->name('notes.create');
+
+    Route::view('create-profile', 'notes.create')
+    ->middleware(['auth'])
+    ->name('notes.create');
+
+    Route::view('students-list', 'notes.index')
+    ->middleware(['auth'])
+    ->name('notes.index');
+
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+    Volt::route('profile/{note}/edit', 'notes.edit-note')
+    ->middleware(['auth'])
+    ->name('notes.edit');
+
+    Route::get('view-profile/{note}', function (Note $note) {
+        if (! $note->is_published) {
+            abort(404);
+        }
+    
+        $user = $note->user;
+    
+        return view('notes.view', ['note' => $note, 'user' => $user]);
+    })->name('notes.view');
 
 require __DIR__.'/auth.php';
