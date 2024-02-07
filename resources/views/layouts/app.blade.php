8a463415c05256e62b2344fa68343e753ff14a26
Leoka2000 <reusoliveiraleo@gmail.com>
2024-02-07 04:57:26 +0100

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
    
     <link rel="icon" type="image/png" href="../../public/logo-top.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
