4a953a47c804b195aa3c83a18e46c26518ed9d77
Leoka2000 <reusoliveiraleo@gmail.com>
2024-03-17 12:33:43 +0100

    emily2

--- resources/views/livewire/notes/referral-reward/referral-reward.blade.php
+++ resources/views/livewire/notes/referral-reward/referral-reward.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <main class='p-3'>
         <x-card title="Congratulations {{ $name }}! You received your reward!">
-            After receiving this email, your next steps will be to wait for our team to reach out to you <br />
-            so we can better discuss this :)
+            After receiving this email, the next steps will be to wait for our team to reach out to you <br />
+            so we can schedule a time for a video chat to explain how everything will work :)
             <br /> <br />
 
             - The email through which we will contact you will be: <strong> {{ $email }}</strong>
